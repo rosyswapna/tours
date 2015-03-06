6aae2fa85ab7fe6db3ab4e0f4552b4e04c88e246
rosyswapna <rosy.swapna@gmail.com>
2015-03-06 11:03:56 +0530

    tour controller

---
+++ application/controllers/tour.php
@@ -0,0 +1,63 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Tour extends CI_Controller {
+	public function __construct()
+	{
+    		parent::__construct();
+		no_cache();
+	}
+
+	public function BusinessSeason()
+	{
+		if($this->session_check()==true) {
+			$data['title']="Business Season | ".PRODUCT_NAME;  
+			$page='user-pages/business-season';
+			$this->load_templates($page,$data);
+		}
+		else{
+			$this->notAuthorized();
+		}	
+	}
+	
+
+
+	public function session_check() {
+		if(($this->session->userdata('isLoggedIn')==true ) && ($this->session->userdata('type')==FRONT_DESK)) {
+			return true;
+		} else {
+			return false;
+		}
+	} 
+
+	public function notAuthorized(){
+		$data['title']='Not Authorized | '.PRODUCT_NAME;
+		$page='not_authorized';
+		$this->load->view('admin-templates/header',$data);
+		$this->load->view('admin-templates/nav');
+		$this->load->view($page,$data);
+		$this->load->view('admin-templates/footer');
+	
+	}
+	public function notFound(){
+		if($this->session_check()==true) {
+		 	$this->output->set_status_header('404'); 
+		 	$data['title']="Not Found";
+      	 		$page='not_found';
+        		 $this->load_templates($page,$data);
+		}else{
+			$this->notAuthorized();
+		}
+	}
+
+	public function load_templates($page='',$data=''){
+		if($this->session_check()==true) {
+			$this->load->view('admin-templates/header',$data);
+			$this->load->view('admin-templates/nav');
+			$this->load->view($page,$data);
+			$this->load->view('admin-templates/footer');
+		}
+		else{
+			$this->notAuthorized();
+		}
+	}
+}

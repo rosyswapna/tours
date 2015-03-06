b24a5af0df89474b553fac88ccf88d4cbe1c7d15
rosyswapna <rosy.swapna@gmail.com>
2015-03-06 17:05:33 +0530

    season and destinations crud frunctions

--- application/controllers/tour.php
+++ application/controllers/tour.php
@@ -6,6 +6,7 @@ class Tour extends CI_Controller {
     		parent::__construct();
 		$this->load->helper('my_helper');
 		$this->load->model('tour_model');
+		$this->load->model('settings_model');
 		no_cache();
 	}
 
@@ -20,9 +21,11 @@ class Tour extends CI_Controller {
 	       			$page='user-pages/user_home';
 				$this->load_templates($page,$data);
 			}elseif($param1=='business-season'){	
-				$this->business_season($param2);
+				$this->show_business_season($param2);
+			}elseif($param1=='manage-business-season'){	
+				$this->manage_business_season($param2);
 			}elseif($param1=='destination'){	
-				$this->destination($param2);
+				$this->show_destination($param2);
 			}else{
 				$this->notFound();
 			}
@@ -32,8 +35,8 @@ class Tour extends CI_Controller {
 		}
 	}
 
-	//-----------------------business season crud----------------------------------
-	public function business_season($id='')
+	//-----------------------business season ----------------------------------
+	public function show_business_season($getID='')
 	{
 		if($this->session_check()==true) {
 
@@ -43,38 +46,17 @@ class Tour extends CI_Controller {
 			$data['ending']= '';
 
 			//if edit get values to form inputs
-			if(is_numeric($id) && $id > 0){
-				$season = $this->tour_model->getBusinessSeason($id);
+			if(is_numeric($getID) && $getID > 0){
+				$season = $this->tour_model->getBusinessSeason($getID);
 				if($season){
 					//get default values for form input values
-					$data['id']= $sesson['id'];
-					$data['name']= $sesson['name'];
-					$data['starting']= $sesson['starting'];
-					$data['ending']= $sesson['ending'];
+					$data['id']= $season['id'];
+					$data['name']= $season['name'];
+					$data['starting']= $season['starting_date'];
+					$data['ending']= $season['ending_date'];
 				}
 			}
-			
-			if(isset($_REQUEST['business-season-add'])){//add season click
-				$dbData['name'] = $_REQUEST['name'];
-				$dbData['starting'] = $_REQUEST['starting'];
-				$dbData['ending'] = $_REQUEST['ending'];
-
-				$this->tour_model->addBusinessSeason($dbData);	
-				
-			}else if(isset($_REQUEST['business-season-edit'])){//edit season click 
-				$dbData['id'] = $_REQUEST['id'];
-				$dbData['name'] = $_REQUEST['name'];
-				$dbData['starting'] = $_REQUEST['starting'];
-				$dbData['ending'] = $_REQUEST['ending'];
-				
-				$this->tour_model->updateBusinessSeason($dbData);
-				
-			}
-			
-			$data['season_list'] = $this->tour_model->getBusinessSeasonList($data);
-
-			$this->tour_model->addBusinessSeason($data);
-
+			$data['season_list'] = $this->tour_model->getBusinessSeasonList();
 			$data['title']="Business Season | ".PRODUCT_NAME;  
 			$page='user-pages/business-season';
 			$this->load_templates($page,$data);
@@ -83,20 +65,127 @@ class Tour extends CI_Controller {
 			$this->notAuthorized();
 		}	
 	}
+
+	//form action
+	public function manage_business_season(){
+		
+		//add or edit
+		if(isset($_REQUEST['business-season-add']) || isset($_REQUEST['business-season-edit'])){
+
+			$this->form_validation->set_rules('name','Season Name','trim|required|xss_clean');
+			$this->form_validation->set_rules('starting','Season Starting','trim|required|xss_clean');
+			$this->form_validation->set_rules('ending','Season Ending','trim|required|xss_clean');
+
+			if($this->form_validation->run() != False) {
+				$dbData['organisation_id'] = $this->session->userdata('organisation_id'); 
+				$dbData['user_id'] = $this->session->userdata('user_id'); 
+				$id = $this->input->post('id');
+				$dbData['name'] = $this->input->post('name');
+				$dbData['starting_date'] = $this->input->post('starting');
+				$dbData['ending_date'] = $this->input->post('ending');
+			
+				if(is_numeric($id) && $id > 0){//edit
+					if($this->settings_model->updateValues('business_seasons',$dbData,$id)){
+						$this->session->set_userdata(array('dbSuccess'=>'Business Season Updated Succesfully..!')); 
+						$this->session->set_userdata(array('dbError'=>''));
+					}
+				}else{//add
+					if($this->settings_model->addValues('business_seasons',$dbData)){
+						$this->session->set_userdata(array('dbSuccess'=>'Business Season Added Succesfully..!')); 
+						$this->session->set_userdata(array('dbError'=>''));
+					}
+				}
+			}		
+			
+		}else if(isset($_REQUEST['business-season-delete'])){//delete season click
+			$id = $this->input->post('id');
+			if($this->settings_model->deleteValues('business_seasons',$id)){
+				$this->session->set_userdata(array('dbSuccess'=>'Business Season Deleted Succesfully..!')); 
+				$this->session->set_userdata(array('dbError'=>''));
+			}
+		}
+
+		$this->show_business_season();
+	}
+
+	
 	//------------------------------------------------------------------------------------------
 	
-	//-----------------------destinations----------------------------------------------------
-	public function destination($id=''){
+	//-----------------------destination ----------------------------------
+	public function show_destination($getID='')
+	{
 		if($this->session_check()==true) {
-			
+
+			$data['id']= '';
+			$data['name']= '';
+			$data['latitude']= '';
+			$data['longitude']= '';
+			$data['seasons']= '';
+
+			//if edit get values to form inputs
+			if(is_numeric($getID) && $getID > 0){
+				$destination = $this->tour_model->getDestination($getID);
+				if($destination){
+					//get default values for form input values
+					$data['id']= $destination['id'];
+					$data['name']= $destination['name'];
+					$data['latitude']= $destination['lat'];
+					$data['longitude']= $destination['lng'];
+					$data['seasons']= $destination['seasons'];
+				}
+			}
+			$data['destination_list'] = $this->tour_model->getDestinationList();
 			$data['title']="Destination | ".PRODUCT_NAME;  
 			$page='user-pages/destination';
 			$this->load_templates($page,$data);
 		}
 		else{
 			$this->notAuthorized();
-		}
+		}	
 	}
+
+	//form action
+	public function manage_destination(){
+		
+		//add or edit
+		if(isset($_REQUEST['destination-add']) || isset($_REQUEST['destination-edit'])){
+
+			$this->form_validation->set_rules('name','Destination Name','trim|required|xss_clean');
+			$this->form_validation->set_rules('lat','Season Starting','numeric|xss_clean');
+			$this->form_validation->set_rules('lng','Season Ending','numeric|xss_clean');
+			if($this->form_validation->run() != False) {
+				$dbData['organisation_id'] = $this->session->userdata('organisation_id'); 
+				$dbData['user_id'] = $this->session->userdata('user_id'); 
+				$id = $this->input->post('id');
+				$dbData['name'] = $this->input->post('name');
+				$dbData['lat'] = $this->input->post('latitude');
+				$dbData['lng'] = $this->input->post('longitude');
+				$dbData['seasons'] = searialize($this->input->post('seasons'));
+			
+				if(is_numeric($id) && $id > 0){//edit
+					if($this->settings_model->updateValues('destinations',$dbData,$id)){
+						$this->session->set_userdata(array('dbSuccess'=>'Destination Updated Succesfully..!')); 
+						$this->session->set_userdata(array('dbError'=>''));
+					}
+				}else{//add
+					if($this->settings_model->addValues('destinations',$dbData)){
+						$this->session->set_userdata(array('dbSuccess'=>'Destination Added Succesfully..!')); 
+						$this->session->set_userdata(array('dbError'=>''));
+					}
+				}
+			}		
+			
+		}else if(isset($_REQUEST['destination-delete'])){//delete season click
+			$id = $this->input->post('id');
+			if($this->settings_model->deleteValues('destinations',$id)){
+				$this->session->set_userdata(array('dbSuccess'=>'Destination Deleted Succesfully..!')); 
+				$this->session->set_userdata(array('dbError'=>''));
+			}
+		}
+
+		$this->show_destination();
+	}
+
 	//------------------------------------------------------------------------------------------
 
 	//----------------------common functions-----------------------------------------
--- application/models/tour_model.php
+++ application/models/tour_model.php
@@ -2,42 +2,14 @@
 class Tour_model extends CI_Model {
 
 	//---------------------------business season start-----------------------------
-	function addBusinessSeason($data = false)
-	{
-		if(!$data)
-			return false;
-		
-		$this->db->set('created', 'NOW()', FALSE);
-		$data['organisation_id'] = $this->session->userdata('organisation_id'); 
-		$data['user_id'] = $this->session->userdata('user_id'); 
-		$this->db->insert('business_seasons',$data);
-		$insert_id = $this->db->insert_id();
-		if($insert_id > 0){
-			return $insert_id;
-		}else{
-			return false;
-		}
-	}
-
-	function updateBusinessSeason($data = false){
-
-		if(!$data)
-			return false;
-		$this->db->set('updated', 'NOW()', FALSE);
-		$this->db->where('id',$id);
-		$this->db->update("business_seasons",$data);
-		return $data['id'];
-		
-		
-	}
-
 	function getBusinessSeason($id)
 	{
+		$this->db->select('id,name,starting_date,ending_date');		
 		$this->db->from('business_seasons');
 		$this->db->where('id',$id);
-		$row = $this->db->get()->row_array();
-		if($this->db->num_rows() == 1){
-			return $row;
+		$query = $this->db->get();
+		if($query->num_rows() == 1){
+			return $query->row_array();
 		}else{
 			return false;
 		}
@@ -45,15 +17,62 @@ class Tour_model extends CI_Model {
 
 	function getBusinessSeasonList()
 	{
+		$this->db->select('id,name,starting_date,ending_date');
 		$this->db->from('business_seasons');
-		$retArray = $this->db->get()->result_array();
-		if($this->db->num_rows() > 0){
+		$this->db->where('organisation_id',$this->session->userdata('organisation_id'));
+		$query = $this->db->get();
+		if($query->num_rows() > 0){
+			return $query->result_array();
+		}else{
+			return false;
+		}
+	}
+	//---------------------------------------------------------
+
+	//---------------------------destinations start-----------------------------
+	
+	function getDestination($id)
+	{
+		$this->db->select('id,name,lat,lng,seasons');
+		$this->db->from('destinations');
+		$this->db->where('id',$id);
+		$query = $this->db->get();
+		$retArray = array();
+		if($query->num_rows() == 1){
+			$row = $query->row();
+			$retArray['id'] = $row->id;
+			$retArray['name'] = $row->name;
+			$retArray['lat'] = $row->lat;
+			$retArray['lng'] = $row->lng;
+			$retArray['seasons'] = unserialize($row->seasons);
 			return $retArray;
 		}else{
 			return false;
 		}
 	}
-	//---------------------------business season ends -----------------------------
+
+	function getDestinationList()
+	{
+		$this->db->select('id,name,lat,lng,seasons');
+		$this->db->from('destinations');
+		$this->db->where('organisation_id',$this->session->userdata('organisation_id'));
+		$query = $this->db->get();
+		$retArray = array();
+		if($query->num_rows() > 0){
+			foreach ($query->result_array() as $row){
+				$retArray[] = array('id' => $row['id'],
+						'name'	=> $row['name'],
+						'lat'	=> $row['lat'],
+						'lng'	=> $row['lng'],
+						'seasons' => unserialize($row['seasons'])
+						);
+			}
+			return $retArray;
+		}else{
+			return false;
+		}
+	}
+	//---------------------------------------------------------
 		
 
 }

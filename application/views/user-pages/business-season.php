1f4898fd2f762f6ecb2c3686a2bf08bddc9fe913
Divya <divyamanoj92@gmail.com>
2015-03-06 11:04:53 +0530

    business season layout

---
+++ application/views/user-pages/business-season.php
@@ -0,0 +1,7 @@
+<div class="business-season-body">
+	<fieldset class="body-border border-style" >
+	<legend class="body-head">Add Business Season</legend>
+		<table>
+		</table>
+	</fieldset>
+</div>

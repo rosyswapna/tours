252a7b9bf6c7adad1aac4af8342d146cabe53961
rosyswapna <rosy.swapna@gmail.com>
2015-03-18 10:58:47 +0530

    trip table updated

--- files/DB/alterations.sql
+++ files/DB/alterations.sql
@@ -3,3 +3,9 @@
 ------------------------------------------------------------------------
 --2015 Mar 11---
 ALTER TABLE `destinations` ADD `status_id` INT(11) NOT NULL AFTER `seasons`;
+
+
+
+--2015 Mar 18---
+ALTER TABLE `trips` CHANGE `from_destination_id` `pick_up` VARCHAR(250) NOT NULL;
+ALTER TABLE `trips` CHANGE `to_destination_id` `drop` VARCHAR(250) NOT NULL;

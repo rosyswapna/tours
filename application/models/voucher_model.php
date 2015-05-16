ecd65f81203fed4cadd579f7ffd4c519d91302d5
rosyswapna <rosy.swapna@gmail.com>
2015-05-16 13:59:02 +0530

    save voucher cart model function

--- application/models/voucher_model.php
+++ application/models/voucher_model.php
@@ -101,15 +101,29 @@ class Voucher_model extends CI_Model {
 		}
 	}
 
+	
+	function updateVoucher($dataArray = false,$id)
+	{
+		if(is_array($dataArray)){
+			$this->db->set('updated', 'NOW()', FALSE);
+			$this->db->update('trip_vouchers', $dataArray);
+			$this->db->where('id',$id)
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+
 	//insert voucher data from cart with trip voucher id
 	function insertVoucherData($voucherData=false,$trip_voucher_id=gINVALID)
 	{	
 		if($voucherData!= false && $trip_voucher_id > 0){
+			//============set insert and update array======
 			foreach($voucherData as $tble => $ItrArray){
-
 				$insertArray = array();
+				$updateArray = array();
 				foreach($ItrArray as $dataArray){
-
 					//if found array values serialize them
 					foreach($dataArray as $colName=>$colVal){
 						if(is_array($colVal)){
@@ -117,15 +131,46 @@ class Voucher_model extends CI_Model {
 						}
 					}
 					$dataArray['trip_voucher_id'] = $trip_voucher_id;
+					$itemId = $dataArray['id'];
 					unset($dataArray['id']);
-					$dataArray['created'] = date("Y-m-d H:i:s");
-					$dataArray['organisation_id'] = $this->session->userdata('organisation_id');
-					$dataArray['user_id'] = $this->session->userdata('id');
-					$insertArray[] = $dataArray;
+					if(is_numeric($itemId) && $itemId > 0){//edit item
+						$dataArray['updated'] = date("Y-m-d H:i:s");
+
+						$updateArray[$tble][$itemId] = $dataArray;
+					}else{//new item
+						$dataArray['created'] = date("Y-m-d H:i:s");
+						$dataArray['organisation_id'] = $this->session->userdata('organisation_id');
+						$dataArray['user_id'] = $this->session->userdata('id');
+						$insertArray[$tble][] = $dataArray;
+					}	
+					
+				}	
+			}
+			//=========================================
+
+			//insert batch
+			if($insertArray){
+				foreach($insertArray as $tbl=>$dataArray){
+					//echo "<pre>";print_r($dataArray);echo "</pre>";exit;
+					$this->db->insert_batch($tbl,$dataArray);
+				
+				}
+			}
+
+			//update batch
+			if($updateArray){
+
+				foreach($updateArray as $tbl=>$dataBatch){
+
+					foreach($dataBatch as $id=>$dataArray){
+					
+						$this->db->where('id', $id);
+						$this->db->set('updated', 'NOW()', FALSE);
+						$this->db->update($tbl, $dataArray); 
+					}
+				
 				}
 				
-				//echo "<pre>";print_r($insertArray);echo "</pre>";exit;
-				$this->db->insert_batch($tble,$insertArray);
 			}
 			return true;
 		}else{
@@ -133,25 +178,59 @@ class Voucher_model extends CI_Model {
 		}
 	}
 
-	//trip voucher save function
-	function saveVoucherCart($voucherCart,$trip_voucher_id = gINVALID)
-	{
-		if($trip_voucher_id == gINVALID){//new voucher
-			//echo "<pre>";print_r($voucherCart);echo "</pre>";exit;
-			$trip_id = $voucherCart->tripId();
-			if(is_numeric($trip_id) && $trip_id > 0){
-				$voucher['trip_id'] = $trip_id;
-				$totals = $voucherCart->totals();
-				foreach($totals as $colName=>$colVal){
-					$voucher[$colName] = $colVal;
-				}
+	function checkVoucherExists($trip_id){
+		
+		$this->db->from('trip_vouchers');
+		$this->db->where('trip_id',$trip_id);
+		$result = $this->db->get();
+		if($result->num_rows() > 0){
+			return $result->row();
+		}else{
+			return false;
+		}
+		
+	}
 
-				//echo "<pre>";print_r($voucher);echo "</pre>";exit;
-				$trip_voucher_id = $this->addVoucher($voucher);
+	//trip voucher save function
+	function saveVoucherCart($voucherCart)
+	{
+		//echo "<pre>";print_r($voucherCart);echo "</pre>";exit;
+		$trip_id = $voucherCart->tripId();
+		if(is_numeric($trip_id) && $trip_id > 0){
+
+			$voucher['trip_id'] = $trip_id;
+			$totals = $voucherCart->totals();
+			foreach($totals as $colName=>$colVal){
+				$voucher[$colName] = $colVal;
+			}
 			
+			if($tourVoucher = checkVoucherExists($trip_id)){//edit
+				if($this->updateVoucher($voucher,$tourVoucher->id))
+					$trip_voucher_id = $tourVoucher->id;
+				else
+					$trip_voucher_id = false;	
+			}else{//new
+				$trip_voucher_id = $this->addVoucher($voucher);
+			}
+				
+			//add or update voucher itineraries
+			if($trip_voucher_id){
 				$voucherData = $voucherCart->contents();
 				$this->insertVoucherData($voucherData,$trip_voucher_id);
-				return true;
+			}
+			return true;
+
+		}else{
+			return false;
+		}
+		
+				
+				
+
+				//echo "<pre>";print_r($voucher);echo "</pre>";exit;
+				
+			
+				
 
 			}else{
 				return false;

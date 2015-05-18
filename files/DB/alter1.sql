fcfb3c340ab713dd5ea37aee482b5b150155e4c7
rosyswapna <rosy.swapna@gmail.com>
2015-05-18 13:12:11 +0530

    two default items added ,accommodation and trip service

--- files/DB/alter1.sql
+++ files/DB/alter1.sql
@@ -6,3 +6,13 @@ ALTER TABLE `trip_vouchers` CHANGE `voucher_number` `voucher_no` VARCHAR(50) CHA
 
 ------18 may 2015---------------
 ALTER TABLE `trip_voucher_vehicles`  ADD `no_of_days` INT(11) NOT NULL AFTER `end_time`;
+
+INSERT INTO `1_stock_master` (`stock_id`, `category_id`, `tax_type_id`, `description`, `long_description`, `units`, `mb_flag`, `sales_account`, `cogs_account`, `inventory_account`, `adjustment_account`, `assembly_account`, `dimension_id`, `dimension2_id`, `actual_cost`, `last_cost`, `material_cost`, `labour_cost`, `overhead_cost`, `inactive`, `no_sale`, `editable`) VALUES
+('104', 1, 1, 'Accommodation', 'Trip accommodation', 'days', 'D', '4010', '5010', '1510', '5040', '1530', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+('105', 1, 1, 'Trip Service', 'trip service', 'hr', 'D', '4010', '5010', '1510', '5040', '1530', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+
+INSERT INTO `1_item_codes` (`id`, `item_code`, `stock_id`, `description`, `category_id`, `quantity`, `is_foreign`, `inactive`) VALUES
+(5, '104', '104', 'Accommodation', 1, 1, 0, 0),
+(6, '105', '105', 'Trip Service', 1, 1, 0, 0);
+
